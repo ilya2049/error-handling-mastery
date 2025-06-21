fcbe19a66191e87d2eaad1ebb13318398dfc4a7e
Simba <tsyganov.ilya62@yandex.ru>
2025-06-21 13:58:10 +0300

    implement 02-allocator-errno_t

--- tasks/02-c-errors-concept/allocator-errno_t/allocator.h
+++ tasks/02-c-errors-concept/allocator-errno_t/allocator.h
@@ -5,7 +5,29 @@ typedef int errno_t;
 #define ADMIN 777
 #define MIN_MEMORY_BLOCK 1024
 
+
 errno_t allocate(int user_id, size_t size, void **mem)
 {
-    // Реализуй меня.
+if (user_id != ADMIN) {
+        *mem = NULL;
+
+        return EPERM;
+    }
+
+    if (size < MIN_MEMORY_BLOCK) {
+        *mem = NULL;
+        
+        return EDOM;
+    }
+
+    void *p = malloc(size);
+    if (p == NULL) {
+        *mem = NULL;
+
+        return ENOMEM;
+    }
+
+    *mem = p;
+
+    return 0;
 }
