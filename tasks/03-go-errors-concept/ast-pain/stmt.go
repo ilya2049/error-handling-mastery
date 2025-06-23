08a71a6af690c875c48a0f36e4b70adbc82dc903
Simba <tsyganov.ilya62@yandex.ru>
2025-06-23 21:42:26 +0300

    implement 03-ast-pain

--- tasks/03-go-errors-concept/ast-pain/stmt.go
+++ tasks/03-go-errors-concept/ast-pain/stmt.go
@@ -7,6 +7,32 @@ import (
 // GetDeferredFunctionName возвращает имя функции, вызов которой был отложен через defer,
 // если входящий node является *ast.DeferStmt.
 func GetDeferredFunctionName(node ast.Node) string {
-	// Реализуй меня.
+	deferStmt, ok := node.(*ast.DeferStmt)
+	if !ok {
+		return ""
+	}
+
+	deferCall := deferStmt.Call
+
+	switch funSelector := deferCall.Fun.(type) {
+	case *ast.SelectorExpr:
+		return getCallableNameAndFunctionName(funSelector)
+	case *ast.Ident:
+		return funSelector.Name
+	case *ast.FuncLit:
+		return "anonymous func"
+	}
+
+	return ""
+}
+
+func getCallableNameAndFunctionName(node *ast.SelectorExpr) string {
+	switch nodeX := node.X.(type) {
+	case *ast.Ident:
+		return nodeX.Name + "." + node.Sel.Name
+	case *ast.SelectorExpr:
+		return getCallableNameAndFunctionName(nodeX) + "." + node.Sel.Name
+	}
+
 	return ""
 }
