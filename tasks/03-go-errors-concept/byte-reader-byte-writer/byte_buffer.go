d081a16a82f80b066673258d852700fc3d2e6617
Simba <tsyganov.ilya62@yandex.ru>
2025-06-24 21:14:18 +0300

    implement 03-byte-reader-byte-writer

--- tasks/03-go-errors-concept/byte-reader-byte-writer/byte_buffer.go
+++ tasks/03-go-errors-concept/byte-reader-byte-writer/byte_buffer.go
@@ -1,12 +1,22 @@
 package bytebuffer
 
+import "fmt"
+
 const bufferMaxSize = 1024
 
 type MaxSizeExceededError struct {
 	desiredLen int
 }
 
+func (e *MaxSizeExceededError) Error() string {
+	return fmt.Sprintf("buffer max size exceeded: %d > %d", e.desiredLen, bufferMaxSize)
+}
+
 type EndOfBufferError struct{}
+
+func (*EndOfBufferError) Error() string {
+	return "end of buffer"
+}
 
 type ByteBuffer struct {
 	buffer []byte
@@ -20,3 +30,29 @@ type ByteBuffer struct {
 //
 // Метод ReadByte должен возвращать ошибку *EndOfBufferError при попытке чтения из буфера,
 // если ранее буфер уже был вычитан полностью.
+func (b *ByteBuffer) ReadByte() (byte, error) {
+	if b.offset == len(b.buffer) {
+		return 0, &EndOfBufferError{}
+	}
+
+	item := b.buffer[b.offset]
+	b.offset++
+
+	return item, nil
+}
+
+func (b *ByteBuffer) WriteByte(c byte) error {
+	if len(b.buffer) == 0 {
+		b.buffer = make([]byte, 0, bufferMaxSize)
+	}
+
+	if len(b.buffer) == bufferMaxSize {
+		return &MaxSizeExceededError{
+			desiredLen: bufferMaxSize + 1,
+		}
+	}
+
+	b.buffer = append(b.buffer, c)
+
+	return nil
+}
