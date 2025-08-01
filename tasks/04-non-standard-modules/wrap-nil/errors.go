d8c7cb0e6f5ff7ed9ea2a74b23f1ca48fd522275
Simba <tsyganov.ilya62@yandex.ru>
2025-08-01 11:16:36 +0300

    implement a task

--- tasks/04-non-standard-modules/wrap-nil/errors.go
+++ tasks/04-non-standard-modules/wrap-nil/errors.go
@@ -1,6 +1,35 @@
 package errors
 
+import "fmt"
+
 // Wrapf работает аналогично fmt.Errorf, только поддерживает nil-ошибки.
 func Wrapf(err error, f string, v ...any) error {
-	return nil
+	if err == nil {
+		return nil
+	}
+
+	return NewError(err, f, v...)
+}
+
+func NewError(err error, f string, v ...any) *Error {
+	message := fmt.Sprintf(f, v...)
+	message += ": " + err.Error()
+
+	return &Error{
+		Cause:   err,
+		Message: message,
+	}
+}
+
+type Error struct {
+	Cause   error
+	Message string
+}
+
+func (e *Error) Error() string {
+	return e.Message
+}
+
+func (e *Error) Unwrap() error {
+	return e.Cause
 }
