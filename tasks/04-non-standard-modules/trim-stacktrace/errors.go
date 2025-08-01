f468824ee79b8318f5b5e358e37b178a93ce230a
Simba <tsyganov.ilya62@yandex.ru>
2025-08-01 20:49:24 +0300

    implement a task

--- tasks/04-non-standard-modules/trim-stacktrace/errors.go
+++ tasks/04-non-standard-modules/trim-stacktrace/errors.go
@@ -2,6 +2,23 @@ package errors
 
 // TrimStackTrace режет все стектрейсы в цепочке ошибок err.
 func TrimStackTrace(err error) error {
-	// Реализуй меня.
-	return err
+	if err != nil {
+		return &Error{
+			cause: err,
+		}
+	}
+
+	return nil
+}
+
+type Error struct {
+	cause error
+}
+
+func (e *Error) Error() string {
+	return e.cause.Error()
+}
+
+func (e *Error) Unwrap() error {
+	return e.cause
 }
