ba69746d47e2982045d40c5f6c8f3007d450258c
Simba <tsyganov.ilya62@yandex.ru>
2025-08-07 20:40:38 +0300

    implement a task

--- tasks/04-non-standard-modules/combine-errors/combine.go
+++ tasks/04-non-standard-modules/combine-errors/combine.go
@@ -1,9 +1,65 @@
 package errors
 
+import (
+	"fmt"
+)
+
 // Combine "прицепляет" ошибки other к err так, что они начинают фигурировать при выводе
 // её на экран через спецификатор `%+v`. Если err является nil, то первостепенной ошибкой
 // становится первая из ошибок other.
 func Combine(err error, other ...error) error {
-	// Реализуй меня.
-	return nil
+	if err == nil {
+		for _, e := range other {
+			if e != nil {
+				return &CombinedError{
+					err: e,
+				}
+			}
+		}
+
+		return nil
+	}
+
+	additionalErrors := make([]error, 0)
+	for _, e := range other {
+		if e != nil {
+			additionalErrors = append(additionalErrors, e)
+		}
+	}
+
+	return &CombinedError{
+		err:              err,
+		additionalErrors: additionalErrors,
+	}
+}
+
+type CombinedError struct {
+	err              error
+	additionalErrors []error
+}
+
+func (e *CombinedError) Error() string {
+	return e.err.Error()
+}
+
+func (e *CombinedError) Unwrap() error {
+	return e.err
+}
+
+func (e *CombinedError) Format(state fmt.State, verb rune) {
+	if verb == 'v' && state.Flag('+') {
+		state.Write([]byte(e.Error()))
+		if len(e.additionalErrors) > 0 {
+			state.Write([]byte("\n"))
+		}
+
+		for _, ad := range e.additionalErrors {
+			state.Write([]byte("  - " + ad.Error()))
+			state.Write([]byte("\n"))
+		}
+
+		return
+	}
+
+	state.Write([]byte(e.Error()))
 }
--- tasks/04-non-standard-modules/is-any/is_any.go
+++ tasks/04-non-standard-modules/is-any/is_any.go
@@ -1,6 +1,13 @@
 package errors
 
+import "errors"
+
 func IsAny(err error, references ...error) bool {
-	// Реализуй меня.
+	for _, referenceErr := range references {
+		if errors.Is(err, referenceErr) {
+			return true
+		}
+	}
+
 	return false
 }
