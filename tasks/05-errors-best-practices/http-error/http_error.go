262872258975d14bf8501cff6524ca2575a93bab
Simba <tsyganov.ilya62@yandex.ru>
2025-08-09 17:52:03 +0300

    implement a task

--- tasks/05-errors-best-practices/http-error/http_error.go
+++ tasks/05-errors-best-practices/http-error/http_error.go
@@ -1,13 +1,28 @@
 package httperr
 
+import (
+	"fmt"
+	"net/http"
+)
+
 // Реализуй нас.
 var (
-	ErrStatusOK                  error
-	ErrStatusBadRequest          error
-	ErrStatusNotFound            error
-	ErrStatusUnprocessableEntity error
-	ErrStatusInternalServerError error
+	ErrStatusOK                  = HTTPError(200)
+	ErrStatusBadRequest          = HTTPError(400)
+	ErrStatusNotFound            = HTTPError(404)
+	ErrStatusUnprocessableEntity = HTTPError(422)
+	ErrStatusInternalServerError = HTTPError(500)
 )
 
 // Реализуй меня.
 type HTTPError int
+
+func (e HTTPError) Code() int {
+	return int(e)
+}
+
+func (e HTTPError) Error() string {
+	code := e.Code()
+
+	return fmt.Sprintf("%d %s", code, http.StatusText(code))
+}
