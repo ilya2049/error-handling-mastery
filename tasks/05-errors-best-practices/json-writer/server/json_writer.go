835b83d0611396559c24d68751803c4e561bc60d
Simba <tsyganov.ilya62@yandex.ru>
2025-08-20 20:30:05 +0300

    add task implementations

--- tasks/05-errors-best-practices/json-writer/server/json_writer.go
+++ tasks/05-errors-best-practices/json-writer/server/json_writer.go
@@ -1,8 +1,27 @@
 package server
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 // Реализуй меня.
-type jsonWriter struct{}
+type jsonWriter struct {
+	log ILogger
+	w   http.ResponseWriter
+}
 
 func (jw jsonWriter) Write(v any) {
-	// Реализуй меня.
+	bytes, err := json.Marshal(v)
+	if err != nil {
+		jw.log.Error(err.Error())
+
+		jw.w.WriteHeader(http.StatusInternalServerError)
+		jw.w.Write([]byte(err.Error()))
+
+		return
+	}
+
+	jw.w.Header().Set("content-type", "application/json")
+	jw.w.Write(bytes)
 }
--- tasks/05-errors-best-practices/read-by-chunk/reader.go
+++ tasks/05-errors-best-practices/read-by-chunk/reader.go
@@ -1,9 +1,67 @@
 package reader
 
-import "io"
+import (
+	"errors"
+	"io"
+)
 
 var ErrInvalidChunkSize error
 
 func ReadByChunk(r io.Reader, chunkSize int) ([][]byte, error) {
-	return nil, nil
+	if r == nil {
+		return nil, nil
+	}
+
+	if chunkSize < 1 {
+		return nil, ErrInvalidChunkSize
+	}
+
+	var (
+		chunks    = NewChunks(chunkSize)
+		err       error
+		readBytes int
+	)
+
+	for err == nil {
+		buf := make([]byte, chunkSize)
+		readBytes, err = r.Read(buf)
+		chunks.Append(buf[:readBytes])
+	}
+
+	if errors.Is(err, io.EOF) {
+		return chunks.Flush(), nil
+	}
+
+	return nil, err
+}
+
+type Chunks struct {
+	chunkSize int
+	chunks    [][]byte
+	tmpChunk  []byte
+}
+
+func NewChunks(chunkSize int) *Chunks {
+	return &Chunks{
+		chunkSize: chunkSize,
+		chunks:    make([][]byte, 0),
+		tmpChunk:  make([]byte, 0, chunkSize),
+	}
+}
+
+func (ch *Chunks) Append(bytes []byte) {
+	for _, b := range bytes {
+		if len(ch.tmpChunk) == ch.chunkSize {
+			ch.chunks = append(ch.chunks, ch.tmpChunk)
+			ch.tmpChunk = make([]byte, 0, ch.chunkSize)
+		}
+
+		ch.tmpChunk = append(ch.tmpChunk, b)
+	}
+}
+
+func (ch *Chunks) Flush() [][]byte {
+	ch.chunks = append(ch.chunks, ch.tmpChunk)
+
+	return ch.chunks
 }
