bde24ca12d488611cc5f303d3c4c09b5b66f84e2
Simba <tsyganov.ilya62@yandex.ru>
2025-08-21 20:42:11 +0300

    implement a task

--- tasks/05-errors-best-practices/pretty-error/pretty.go
+++ tasks/05-errors-best-practices/pretty-error/pretty.go
@@ -1,8 +1,55 @@
 package errs
 
+import (
+	"errors"
+	"strings"
+)
+
 // Pretty делает цепочку ошибок более читаемой – очередная заврапленная
 // ошибка будет представлена на новой строке.
 func Pretty(err error) error {
-	// Реализуй меня.
-	return nil
+	if err == nil {
+		return nil
+	}
+
+	prevErr := err
+
+	messages := make([]string, 0)
+
+	for {
+		curErr := errors.Unwrap(prevErr)
+		if curErr != nil {
+			messages = append(messages, cut(prevErr.Error(), curErr.Error()))
+		} else {
+			messages = append(messages, prevErr.Error())
+
+			break
+		}
+
+		prevErr = curErr
+	}
+
+	return &PrettyError{
+		message: strings.Join(messages, "\n"),
+		cause:   err,
+	}
+}
+
+func cut(str, substr string) string {
+	index := strings.Index(str, substr)
+
+	return strings.TrimSpace(str[:index])
+}
+
+type PrettyError struct {
+	message string
+	cause   error
+}
+
+func (e *PrettyError) Error() string {
+	return e.message
+}
+
+func (e *PrettyError) Unwrap() error {
+	return e.cause
 }
