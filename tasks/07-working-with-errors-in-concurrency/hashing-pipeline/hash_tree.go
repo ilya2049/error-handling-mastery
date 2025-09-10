94a61430af4736a95ba3f8d077cdcc71f26cf8ac
Simba <tsyganov.ilya62@yandex.ru>
2025-09-10 20:05:40 +0300

    implement a task

--- tasks/07-working-with-errors-in-concurrency/hashing-pipeline/hash_tree.go
+++ tasks/07-working-with-errors-in-concurrency/hashing-pipeline/hash_tree.go
@@ -7,16 +7,39 @@ var errNothingToHash = errors.New("nothing to hash")
 // CalculateHash реализует хеширование входящих элементов по принципу дерева хешей (Merkle tree).
 // Если входящий слайс пуст, то возвращает ошибку errNothingToHash.
 func CalculateHash(hh []Hashable) (Hash, error) {
-	// Реализуй меня. При желании воспользуйся node.
-	return nil, nil
+	if len(hh) == 0 {
+		return nil, errNothingToHash
+	}
+
+	hashes := make([]Hash, len(hh))
+	for i, h := range hh {
+		hashes[i] = h.Hash()
+	}
+
+	return calculateHash(hashes), nil
 }
 
-// node представляет собой узел хеш-дерева.
-type node struct {
-	left  Hashable
-	right Hashable
-}
+func calculateHash(hh []Hash) Hash {
+	var newHashesLength int
+	if len(hh)%2 == 0 {
+		newHashesLength = len(hh) / 2
+	} else {
+		newHashesLength = len(hh)/2 + 1
+	}
 
-func (n node) Hash() Hash {
-	return newHash(append(n.left.Hash(), n.right.Hash()...))
+	newHashes := make([]Hash, 0, newHashesLength)
+
+	for i := 0; i < len(hh); i += 2 {
+		if i < len(hh)-1 {
+			newHashes = append(newHashes, newHash(append(hh[i], hh[i+1]...)))
+		} else {
+			newHashes = append(newHashes, newHash(append(hh[i], hh[i]...)))
+		}
+	}
+
+	if len(newHashes) > 1 {
+		return calculateHash(newHashes)
+	}
+
+	return newHashes[0]
 }
--- tasks/07-working-with-errors-in-concurrency/hashing-pipeline/pipeline.go
+++ tasks/07-working-with-errors-in-concurrency/hashing-pipeline/pipeline.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"io"
 	"strconv"
+	"sync"
 )
 
 const maxBatchSize = 4
@@ -39,7 +40,64 @@ type Block struct {
 //     досрочного завершения и ожидает их);
 //   - при безошибочной обработке всех транзакций.
 func Pipeline(ctx context.Context, batchSize int, out io.Writer, txs ...Transaction) error {
-	// Реализуй меня.
+	drainErrChan := func(errs <-chan error) {
+		for range errs {
+		}
+	}
+
+	var errChans []<-chan error
+
+	batchChan, batchErrChan, err := batch(ctx, batchSize, txs...)
+	if err != nil {
+		return err
+	}
+
+	errChans = append(errChans, batchErrChan)
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	blockChan, blockErrChan, err := hashTransactions(ctx, batchChan)
+	if err != nil {
+		cancel()
+
+		outErrChan, _ := mergeErrors(errChans...)
+		drainErrChan(outErrChan)
+
+		return err
+	}
+
+	errChans = append(errChans, blockErrChan)
+
+	sinkErrChan, err := sink(ctx, blockChan, out)
+	if err != nil {
+		cancel()
+
+		outErrChan, _ := mergeErrors(errChans...)
+		drainErrChan(outErrChan)
+
+		return err
+	}
+
+	errChans = append(errChans, sinkErrChan)
+
+	outErrChan, err := mergeErrors(errChans...)
+	if err != nil {
+		cancel()
+
+		drainErrChan(outErrChan)
+
+		return err
+	}
+
+	for err := range outErrChan {
+		cancel()
+
+		drainErrChan(outErrChan)
+
+		return err
+	}
+
 	return nil
 }
 
@@ -49,24 +107,126 @@ func Pipeline(ctx context.Context, batchSize int, out io.Writer, txs ...Transact
 //   - если у очередной транзакции нулевой ID, то функция пишет в выходной канал ошибку errEmptyTx
 //     и завершает своё выполнение.
 func batch(ctx context.Context, batchSize int, txs ...Transaction) (<-chan []Transaction, <-chan error, error) {
-	// Реализуй меня.
-	return nil, nil, nil
+	if batchSize < 1 || batchSize > maxBatchSize {
+		return nil, nil, errInvalidBatchSize
+	}
+
+	if len(txs) == 0 {
+		return nil, nil, errEmptyInput
+	}
+
+	txsChan := make(chan []Transaction)
+	errChan := make(chan error, 1)
+
+	go func() {
+		batch := make([]Transaction, 0, batchSize)
+
+		defer close(txsChan)
+		defer close(errChan)
+
+		for _, tx := range txs {
+			if tx.ID == 0 {
+				errChan <- errEmptyTx
+
+				return
+			}
+
+			batch = append(batch, tx)
+			if len(batch) == cap(batch) {
+				select {
+				case <-ctx.Done():
+					return
+				case txsChan <- batch:
+					batch = make([]Transaction, 0, batchSize)
+				}
+			}
+		}
+
+		if len(batch) > 0 {
+			select {
+			case <-ctx.Done():
+				return
+			case txsChan <- batch:
+			}
+		}
+	}()
+
+	return txsChan, errChan, nil
 }
 
 // hashTransactions берёт группы транзакций из входного канала, и считает хеш от группы с помощью CalculateHash:
 //   - возвращает ошибку errNilChannel, если на вход получила nil-канал;
 //   - если при просчёте хеша возникает ошибка, то функция пишет её в выходной канал и завершает своё выполнение.
 func hashTransactions(ctx context.Context, batchc <-chan []Transaction) (<-chan Block, <-chan error, error) {
-	// Реализуй меня.
-	return nil, nil, nil
+	if batchc == nil {
+		return nil, nil, errNilChannel
+	}
+
+	errChan := make(chan error, 2)
+	blockc := make(chan Block)
+
+	txsToHashable := func(txs []Transaction) []Hashable {
+		hashable := make([]Hashable, 0, len(txs))
+
+		for _, tx := range txs {
+			hashable = append(hashable, tx)
+		}
+
+		return hashable
+	}
+
+	go func() {
+		defer close(blockc)
+		defer close(errChan)
+
+		for batch := range batchc {
+			hash, err := CalculateHash(txsToHashable(batch))
+			if err != nil {
+				errChan <- err
+
+				return
+			}
+
+			select {
+			case <-ctx.Done():
+				return
+			case blockc <- Block{Hash: hash}:
+			}
+		}
+	}()
+
+	return blockc, errChan, nil
 }
 
 // sink выводит в out хеши блоков из входного канала в строковой форме, разделяя их через '\n':
 //   - возвращает ошибку errNilChannel, если на вход получила nil-канал;
 //   - если при записи в out возникает ошибка, то функция пишет её в выходной канал и завершает своё выполнение.
 func sink(ctx context.Context, blockc <-chan Block, out io.Writer) (<-chan error, error) {
-	// Реализуй меня.
-	return nil, nil
+	if blockc == nil {
+		return nil, errNilChannel
+	}
+
+	errChan := make(chan error, 1)
+
+	go func() {
+		defer close(errChan)
+
+		for block := range blockc {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				outString := block.Hash.String() + "\n"
+				if _, err := out.Write([]byte(outString)); err != nil {
+					errChan <- err
+
+					return
+				}
+			}
+		}
+	}()
+
+	return errChan, nil
 }
 
 // mergeErrors сливает все ошибки из входящих каналов в выходной канал:
@@ -74,6 +234,34 @@ func sink(ctx context.Context, blockc <-chan Block, out io.Writer) (<-chan error
 //   - возвращает ошибку errNilChannel, если хотя бы один из каналов в слайсе нулевой;
 //   - выходной канал закрывается только после того, как будут вычитаны все входные каналы.
 func mergeErrors(errcs ...<-chan error) (<-chan error, error) {
-	// Реализуй меня.
-	return nil, nil
+	if len(errcs) == 0 {
+		return nil, errEmptyInput
+	}
+
+	for _, ch := range errcs {
+		if ch == nil {
+			return nil, errNilChannel
+		}
+	}
+
+	var wg sync.WaitGroup
+
+	output := make(chan error)
+
+	for _, ch := range errcs {
+		wg.Add(1)
+		go func(ch <-chan error) {
+			for err := range ch {
+				output <- err
+			}
+			wg.Done()
+		}(ch)
+	}
+
+	go func() {
+		wg.Wait()
+		close(output)
+	}()
+
+	return output, nil
 }
